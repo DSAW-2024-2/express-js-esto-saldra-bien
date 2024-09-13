80bbf9797d701f696ac6680e6df4db13afe68b1d
Santiago Bazzani RIncon <Santiagobazzanir@gmail.com>
2024-09-12 19:14:06 -0500

    primera parte

---
+++ java-express/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const PORT = 3000; 
+
+const students = [
+  {
+    id: 1,
+    name: "Santiago",          
+    lastName: "Bazzani",      
+    email: "santiagobari@unisabana.edu.co",  
+    id: "0000310457"           
+  },
+  {
+    id: 2,
+    name: "",           
+    lastName: "",     
+    email: "",  
+    id: ""         
+  }
+];
+
