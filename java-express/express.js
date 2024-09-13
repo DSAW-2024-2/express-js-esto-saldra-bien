69b8d670dc6bc5c8ff7b0ca67ce5d67f687b64b2
Santiago Bazzani RIncon <Santiagobazzanir@gmail.com>
2024-09-12 21:24:46 -0500

    Add files via upload

---
+++ java-express/express.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+const students = {
+  1: {
+    name: "Santiago",
+    lastName: "Bazzani",
+    email: "santiagobari@unisabana.edu.co",
+    id: "0000310457"
+  },
+  2: {
+    name: "Daniel",
+    lastName: "Forero",
+    email: "danielforsa@unisabana.edu.co",
+    id: "0000309277"
+  }
+};
+
+app.get('/user-info/:id', (req, res) => {
+  const studentId = req.params.id;
+
+  if (!students[studentId]) {
+    return res.status(404).json({ error: "Estudiante no encontrado" });
+  }
+
+  res.json(students[studentId]);
+});
+
+app.listen(port, () => {
+  console.log(`Servidor escuchando en http://localhost:${port}`);
+});
